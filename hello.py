a7338aa9b0185fd8cda9a6c57f1bed1f5a8b5484
NicholasYao <43372380+aircov@users.noreply.github.com>
2018-12-10 21:07:30 +0800

    123

---
+++ hello.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Administrator
+# @Date:   2018-12-10 21:04:11
+# @Last Modified by:   Administrator
+# @Last Modified time: 2018-12-10 21:04:52
+print('hello world')
